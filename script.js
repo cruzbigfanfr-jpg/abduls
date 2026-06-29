f388040820f2af736490f027071438525a33abfb
cruzbigfanfr-jpg <cruzbigfanfr@gmail.com>
2026-06-29 10:20:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+// Smooth page loading
+window.addEventListener("load", () => {
+  document.body.style.opacity = "1";
+});
+
+// Button click animation
+document.querySelectorAll("button").forEach(button => {
+  button.addEventListener("click", () => {
+    button.style.transform = "scale(0.95)";
+
+    setTimeout(() => {
+      button.style.transform = "scale(1)";
+    }, 150);
+  });
+});
+
+// Console Message
+console.log("ABDULS Website Loaded Successfully!");
